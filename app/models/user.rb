12beb11d9968038f07c295ef1ea567ddbb34e915
Daniel <x15004091@student.ncirl.ir>
2019-03-17 17:31:52 +0000

    Replaced Event placeholder with live data.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   enum user_level: { unassigned: 0, employee: 1, manager: 2, admin: 3 }
 
-  has_many :time_offs, through: :approvals
+  #has_many :time_offs, through: :approvals
+  has_many :time_offs
 end
