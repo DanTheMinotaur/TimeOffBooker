841d3a9605d72d1a1e6b5e003f8bc2b57c03087e
Daniel <x15004091@student.ncirl.ir>
2019-03-17 15:29:25 +0000

    MIgration for not null

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum user_level: { unassigned: 0, employee: 1, manager: 2, admin: 3 }
+
+  has_many :time_offs, through: :approvals
 end
