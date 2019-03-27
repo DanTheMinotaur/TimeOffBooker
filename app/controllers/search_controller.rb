ea1ad2b7874ca9a14ffba51bd66811cd0b855f07
Daniel <x15004091@student.ncirl.ir>
2019-03-27 23:39:08 +0000

    Search Styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,8 @@ class SearchController < ApplicationController
 
       if search_data.key? :user
         user_search = search_data[:user]
-        @search_user_list = User.joins(:time_offs).where('name LIKE ?', "%#{user_search}%")
+        @search_key = user_search
+        @search_user_list = User.joins(:time_offs).distinct.where('name LIKE ?', "%#{user_search}%")
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'requests', to: 'dashboard#requests'
 
   post 'search', to: 'search#index'
+  get 'search', to: 'search#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
