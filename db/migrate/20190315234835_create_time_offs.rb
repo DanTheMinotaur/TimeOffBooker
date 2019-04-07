c3e6872a3a4600ef772f44fa6ec7fbf4d0559586
Daniel <x15004091@student.ncirl.ir>
2019-04-07 18:13:17 +0100

    Altered Migrations for not null for PostGres

--- db/migrate/20190315234835_create_time_offs.rb
+++ db/migrate/20190315234835_create_time_offs.rb
@@ -6,7 +6,7 @@ class CreateTimeOffs < ActiveRecord::Migration[5.2]
       t.text :note
       t.integer :off_type, null: false, default: 0
       t.belongs_to :user, index: true
-      t.boolean :approved
+      t.boolean :approved, null: true
       t.integer :approved_by, null: false
       t.timestamps
     end
